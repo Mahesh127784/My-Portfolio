13c66a7e163009e315c72c4a9d0f887fc044ab24
MaheshNaik <149051484+Mahesh127784@users.noreply.github.com>
2024-03-18 23:20:35 +0530

    Update scrypt.js

--- scrypt.js
+++ scrypt.js
@@ -147,7 +147,7 @@ for (let i = 0; i < icons.length; i++) {
       window.open("https://github.com/Mahesh127784", "_blank");
     if (icon.contains("linkedIn"))
       window.open(
-        "https://www.linkedin.com/in/mahesh-naik-7035a22a0?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app ",
+        "https://www.linkedin.com/in/mahesh-naik-7035a22a0 ",
         "_blank"
       );
     if (icon.contains("twitter"))
