ebc988b116579d62093438f8dedff2428983c98e
MaheshNaik <mn127184@gmail.com>
2024-06-28 18:37:49 +0530

    completed projects section

--- scrypt.js
+++ scrypt.js
@@ -1,8 +1,18 @@
 let navLinks = document.querySelectorAll("nav ul li a");
 let navList = document.querySelector(".allLists");
 let listBar = document.querySelector(".listbar");
+let projectsContainer = document.querySelector(".allProjects");
 
-// on loading the page adding the listener to lav links
+// listener for title change
+document.addEventListener("visibilitychange", function () {
+  if (document.visibilityState === "visible") {
+    document.title = "Portfolio | Mahesh Naik";
+  } else {
+    document.title = "Come Back To Portfolio";
+  }
+});
+
+// on loading the page adding the listener to nav links
 
 document.addEventListener("DOMContentLoaded", () => {
   // Set initial active link
@@ -60,6 +70,27 @@ listBar.addEventListener("click", (e) => {
   navList.style.display = "flex";
 });
 
+// Adding skills box dinamically using javascript forEach loop
+
+function displaySkills(allSkills) {
+  let skillsContainer = document.querySelector(".skillcontainer");
+
+  allSkills.forEach((skill) => {
+    let elem = document.createElement("div");
+    elem.className = "allSkills";
+
+    let childelem1 = document.createElement("img");
+    childelem1.src = skill.icon;
+    childelem1.className = "skillicons";
+    let childelem2 = document.createElement("p");
+    childelem2.innerHTML = skill.name;
+
+    elem.appendChild(childelem1);
+    elem.appendChild(childelem2);
+    skillsContainer.appendChild(elem);
+  });
+}
+
 // array of images and names of skills
 let skills = [
   {
@@ -75,8 +106,17 @@ let skills = [
     icon: "https://img.icons8.com/color/48/000000/javascript--v1.png",
   },
   {
+    name: "TypeScript",
+    icon: "https://img.icons8.com/color/48/000000/typescript--v1.png",
+  },
+
+  {
     name: "ReactJS",
     icon: "https://img.icons8.com/external-tal-revivo-color-tal-revivo/48/000000/external-react-a-javascript-library-for-building-user-interfaces-logo-color-tal-revivo.png",
+  },
+  {
+    name: "NextJS",
+    icon: "https://www.drupal.org/files/project-images/nextjs-icon-dark-background.png",
   },
   {
     name: "ExpressJS",
@@ -94,48 +134,106 @@ let skills = [
     name: "MongoDB",
     icon: "https://img.icons8.com/color/48/000000/mongodb.png",
   },
-  {
-    name: "MySQL",
-    icon: "https://img.icons8.com/color/48/000000/mysql-logo.png",
-  },
+  // {
+  //   name: "MySQL",
+  //   icon: "https://img.icons8.com/color/48/000000/mysql-logo.png",
+  // },
 
   {
     name: "TailwindCSS",
     icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/48px-Tailwind_CSS_Logo.png",
   },
-  {
-    name: "Bootstrap",
-    icon: "https://img.icons8.com/color/48/000000/bootstrap.png",
-  },
-
   // {
   //   name: "jQuery",
   //   icon: "https://img.icons8.com/ios-filled/48/1169ae/jquery.png",
   // },
-
   {
     name: "GitHub",
     icon: "https://img.icons8.com/glyph-neue/48/ffffff/github.png",
   },
+  {
+    name: "Heroku",
+    icon: "https://img.icons8.com/color/48/000000/heroku.png",
+  },
+  {
+    name: "Netlify",
+    icon: "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/48/000000/external-netlify-a-cloud-computing-company-that-offers-hosting-and-serverless-backend-services-for-static-websites-logo-shadow-tal-revivo.png",
+  },
+  {
+    name: "DigitalOcean",
+    icon: "https://img.icons8.com/ios-filled/48/0080FF/digitalocean.png",
+  },
 ];
 
-// Adding skills box dinamically using javascript forEach loop
-let skillsDiv = document.querySelector(".skillcontainer");
+displaySkills(skills);
 
-skills.forEach((skill) => {
-  let elem = document.createElement("div");
-  elem.className = "allSkills";
+function displayProjects(allProjects) {
+  let projectHTML = "";
 
-  let childelem1 = document.createElement("img");
-  childelem1.src = skill.icon;
-  childelem1.className = "skillicons";
-  let childelem2 = document.createElement("p");
-  childelem2.innerHTML = skill.name;
+  allProjects.forEach((project) => {
+    projectHTML += `
+        <div class="box">
+      <img draggable="false" src="/images/${project.image}.png" alt="project" />
+      <div class="content">
+        <div class="tag">
+        <h3>${project.name}</h3>
+        </div>
+        <div class="desc">
+          <p>${project.desc}</p>
+          <div class="btns">
+            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+           
+          </div>
+        </div>
+      </div>
+    </div>`;
+  });
+  projectsContainer.innerHTML = projectHTML;
+}
 
-  elem.appendChild(childelem1);
-  elem.appendChild(childelem2);
-  skillsDiv.appendChild(elem);
-});
+const allProjects = [
+  {
+    name: "School Administration App [MERN]",
+    desc: "A Full Stack MERN Application to Handle a School Management.",
+    image: "schoolapp",
+    links: {
+      view: "https://giis-administration.vercel.app/",
+      code: "https://github.com/Mahesh127784/School_Administration-frontEnd",
+      backend: "https://github.com/Mahesh127784/school_Administration-backend",
+    },
+  },
+  {
+    name: "Music School [NEXT]",
+    desc: "A Client Side Music School's App Made In NextJS And Uncooked Framework For UI.",
+    image: "musicschool",
+    links: {
+      view: "https://your-music-school.vercel.app/",
+      code: "https://github.com/Mahesh127784/MusicSchool-nextJS",
+    },
+  },
+  {
+    name: "e-Notebook [MERN]",
+    desc: "A Full Stack MERN Application Which Works As A Notebook To Store Users Note.",
+    image: "enotebook",
+    links: {
+      view: "https://notebook-oncloud.netlify.app/",
+      code: "https://github.com/Mahesh127784/e-NOTEBOOK-frontend",
+      backend: "https://github.com/Mahesh127784/e-NOTEBOOK-Backend",
+    },
+  },
+  {
+    name: "Number-Bubble Game [JS]",
+    desc: "A Number Game Made up of HTML CSS and JAVASCRIPT Which make Users Brain",
+    image: "bubblegame",
+    links: {
+      view: "https://bubble-numbergame.netlify.app/",
+      code: "https://github.com/Mahesh127784/Bubble-Number_Game",
+    },
+  },
+];
+
+displayProjects(allProjects);
 
 // Redirecting the page which icons are reflecting to
 let icons = document.getElementsByClassName("icons");
@@ -147,7 +245,7 @@ for (let i = 0; i < icons.length; i++) {
       window.open("https://github.com/Mahesh127784", "_blank");
     if (icon.contains("linkedIn"))
       window.open(
-        "https://www.linkedin.com/in/mahesh-naik-7035a22a0?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app ",
+        "https://www.linkedin.com/in/mahesh-naik-7035a22a0",
         "_blank"
       );
     if (icon.contains("twitter"))
@@ -163,7 +261,8 @@ let Up = true;
 
 setInterval(() => {
   if (!Up) {
-    heart.style.transform = "scale(1.1)";
+    heart.style.transform =
+      "scale(1.1                                                       )";
   } else {
     heart.style.transform = "scale(0.8)";
   }
